1cf28e94c6377252eeefd5fa83693ac34014308c
kingdanie <daniedemola@gmail.com>
2024-04-06 14:21:08 +0100

    fetch tasks to dashbaord

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NagivationComponent } from '../nagivation/nagivation.component';
 import { DatePipe } from '@angular/common';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../interfaces/task.model';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +14,25 @@ import { DatePipe } from '@angular/common';
 })
 export class DashboardComponent {
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.loadTasks();
+
+    this.taskService.onDataUpdate().subscribe(() => {
+      this.loadTasks();
+    });
+  }
+  latestTasks: Task[] = [];
   currentDate = new Date();
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe(tasks => {
+      // Filter tasks into different arrays based on status
+      this.latestTasks = tasks.slice(0, 4); // Display the first 5 tasks
+      console.log(tasks);
+    });
+  }
 
 
 }
