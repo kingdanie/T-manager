2a3e42ca99c0e09baec75b3df4c538cdd2f61529
kingdanie <daniedemola@gmail.com>
2024-04-06 14:19:15 +0100

    added memebers component

--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { PageHeaderComponent } from "../page-header/page-header.component";
 
 @Component({
-  selector: 'app-members',
-  standalone: true,
-  imports: [],
-  templateUrl: './members.component.html',
-  styleUrl: './members.component.css'
+    selector: 'app-members',
+    standalone: true,
+    templateUrl: './members.component.html',
+    styleUrl: './members.component.css',
+    imports: [PageHeaderComponent]
 })
 export class MembersComponent {
 
