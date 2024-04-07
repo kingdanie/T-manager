65e7c46c8b9707d435ca9845d5b78e6f2da3a9a2
kingdanie <daniedemola@gmail.com>
2024-04-07 19:31:12 +0100

    added calendar icon

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -2,15 +2,19 @@ import { Component, Input } from '@angular/core';
 import { Task } from '../../interfaces/task.model';
 import { ModalService } from '../../services/modal.service';
 import { TaskService } from '../../services/task.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendar, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  
+  imports: [FontAwesomeModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+
+  faCalendar = faCalendarAlt;
   @Input()
   task!: Task;
 
